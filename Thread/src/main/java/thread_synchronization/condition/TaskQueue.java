0ea2ddccd8af7a171d3e5391f87115b7bedf73f3
ZhiHao <2854748077@qq.com>
2024-09-01 09:43:30 +0800

    *

--- Thread/src/main/java/thread_synchronization/condition/TaskQueue.java
+++ Thread/src/main/java/thread_synchronization/condition/TaskQueue.java
@@ -1,5 +1,17 @@
 package thread_synchronization.condition;
 
-public class TaskQueue {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+public class TaskQueue {
+    private final Lock lock=new ReentrantLock();
+    private final Condition condition=lock.newCondition();
+    private Queue<String> TaskQueue=new LinkedList<>();
+
+    public Queue<String> getTaskQueue(){
+
+    }
 }
